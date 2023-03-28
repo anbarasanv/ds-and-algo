3914eda90cd803c5ed18262ff32dad5a5568f98a
anbarasanv <anbarasan.mailme@gmail.com>
2023-03-28 09:11:33 +0530

    updated-linked-list-code

--- linked-list/linked-list-ds.py
+++ linked-list/linked-list-ds.py
@@ -1,12 +1,17 @@
-"""The LinkedList code from before is provided below.
-Add three functions to the LinkedList.
-"get_position" returns the element at a certain position.
-The "insert" function will add an element to a particular
-spot in the list.
-"delete" will delete the first element with that
-particular value.
-Then, use "Test Run" and "Submit" to run the test cases
-at the bottom."""
+""" 
+  ┌────────────────────────────────────────────────────────────────────────────┐
+  │ The LinkedList code from before is provided below.                         │
+  │ Add three functions to the LinkedList.                                     │
+  │ "get_position" returns the element at a certain position.                  │
+  │ The "insert" function will add an element to a particular                  │
+  │ spot in the list.                                                          │
+  │ "delete" will delete the first element with that                           │
+  │ particular value.                                                          │
+  │ Then, use "Test Run" and "Submit" to run the test cases                    │
+  │ at the bottom.                                                             │
+  │                                                                            │
+  └────────────────────────────────────────────────────────────────────────────┘
+ """
 
 
 class Element(object):
@@ -91,21 +96,14 @@ ll.append(e3)
 
 # Test get_position
 # Should print 3
-print(ll.head.next.next.value)
 # Should also print 3
-print(ll.get_position(3).value)
 
 # Test insert
 ll.insert(e4, 3)
 # Should print 4 now
-print(ll.get_position(3).value)
 
 # # Test delete
 ll.delete(1)
-print("########Delete#############")
 # Should print 2 now
-print(ll.get_position(1).value)
 # Should print 4 now
-print(ll.get_position(2).value)
 # Should print 3 now
-print(ll.get_position(3).value)
